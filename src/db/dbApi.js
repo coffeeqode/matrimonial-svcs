df4f858c88d31c8789f8f381bf4982483deb7826
Sandesh Patil <sandesh89@gmail.com>
2017-10-07 20:17:42 +0530

    using heroku services

--- src/db/dbApi.js
+++ src/db/dbApi.js
@@ -2,12 +2,23 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
+    user: 'nupsmafjrohwkq',
+    host: 'ec2-54-163-253-94.compute-1.amazonaws.com',
+    database: 'd3lqc0ad8csf0n',
+    password: 'c016782212e9240c7bd1b58aa2a9ff84d7b228608035468dd7a7974988859759',
+    port: 5432,
+    ssl: true
+})
+
+
+/* const pool = new Pool({
     user: 'sandesh',
     host: 'localhost',
     database: 'matrimonial',
     password: 'sandesh',
     port: 5432,
 })
+ */
 
 var query = function (text, params, callback) {
     return pool.query(text, params, callback)
